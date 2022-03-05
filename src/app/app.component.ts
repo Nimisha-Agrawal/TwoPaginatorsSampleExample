5bd1641553500b8fe23a6d542216eb58ebfa9af0
Nimisha Agrawal <nimishagrawal.121@gmail.com>
2022-03-05 15:23:40 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource} from '@angular/material/table';
+import { Model } from './interfaces/data.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'TwoPaginators';
+  public displayedColumns = ['id', 'name', 'email', 'role'];
+  public dataSource = new MatTableDataSource<Model>();
+
+  @ViewChild(MatSort)
+  sort!: MatSort;
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+ 
+  constructor(private http: HttpClient) { }
+ 
+  ngOnInit() {
+    this.getAllOwners();
+  }
+ 
+  public getAllOwners = () => {
+    this.http.get('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json')
+    .subscribe(res => {
+      this.dataSource.data = res as Model[];
+    })
+  }
+
+  ngAfterViewInit(){
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  
+  public customSort = (event:any) => {
+    console.log(event);
+  }
+ 
+  //A method to sync the first paginator, if the second is used:
+  syncTopPaginator(event:PageEvent){
+    this.paginator.pageIndex = event.pageIndex;
+    this.paginator.pageSize = event.pageSize;
+    this.paginator.page.emit(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './material/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
